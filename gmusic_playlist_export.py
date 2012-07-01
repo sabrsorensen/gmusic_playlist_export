eeb320f0bd7c7c7cd697f1884a37db15e0f5343f
Sam Sorensen <sabrsorensen@gmail.com>
2012-07-01 16:15:35 -0600

    Cleaner track info, beginnings of local file location

--- gmusic_playlist_export.py
+++ gmusic_playlist_export.py
@@ -1,23 +1,25 @@
 import gmusicapi
 import getpass
+import os
+from mutagen.mp3 import MP3
 
 api = gmusicapi.Api()
 
-api.login('','')
+account = raw_input('Input your Google Music account:')
+password = getpass.getpass("Password: ")
+api.login(account,password)
+
+musicRoot = raw_input('Enter the location of your music library:')
+
 playlists = api.get_all_playlist_ids()
 
 userlists = playlists.get('user')
 listnames = userlists.keys()
 listids = userlists.values()
 
-input()
-
 for id in listids:
     songs = api.get_playlist_songs(id)
     for song in songs:
-        trackName = song.get('name')
-        trackArtist = song.get('artist')
-        trackAlbumArtist = song.get('albumArtist')
-        trackAlbum = song.get('album')
-        trackNumber = song.get('track')
-        print "Music Library\\" + trackAlbumArtist + '\\' + trackAlbum + '\\' + ("%02d" % trackNumber) + ' - ' + trackArtist + ' - ' + trackName
+        trackInfo = [song.get('albumArtist'),song.get('album'),song.get('artist'),song.get('name'),"%02d" % song.get('track')]
+        print trackInfo
+      #  print "Music Library\\" + trackAlbumArtist + '\\' + trackAlbum + '\\' + ("%02d" % trackNumber) + ' - ' + trackArtist + ' - ' + trackName
