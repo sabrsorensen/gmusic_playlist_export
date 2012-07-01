121d9830338d3cfbf5bd649ef22cec9e0f135660
Murph Murphy <omurcada@gmail.com>
2012-07-01 16:16:29 -0600

    Directory file changes.

--- gmusic_playlist_export.py
+++ gmusic_playlist_export.py
@@ -1,23 +1,27 @@
 import gmusicapi
-import getpass
+from getpass import getpass
+import os
 
 api = gmusicapi.Api()
-
-api.login('','')
+#username = raw_input('Google Music Username: ')
+#password = getpass()
+api.login('omurcada', '99;6(;23*2X4>7^.(9^@6$@4@8$/L/9i<6Jdm764*3g34j$f)p')
 playlists = api.get_all_playlist_ids()
 
 userlists = playlists.get('user')
 listnames = userlists.keys()
 listids = userlists.values()
 
-input()
+music_path = '/Users/Murph/Music/iTunes/iTunes Music/'
 
-for id in listids:
-    songs = api.get_playlist_songs(id)
-    for song in songs:
-        trackName = song.get('name')
-        trackArtist = song.get('artist')
-        trackAlbumArtist = song.get('albumArtist')
-        trackAlbum = song.get('album')
-        trackNumber = song.get('track')
-        print "Music Library\\" + trackAlbumArtist + '\\' + trackAlbum + '\\' + ("%02d" % trackNumber) + ' - ' + trackArtist + ' - ' + trackName
+# for id in listids:
+#     songs = api.get_playlist_songs(id)
+#     for song in songs:
+#         trackName = song.get('name')
+#         trackArtist = song.get('artist')
+#         trackAlbumArtist = song.get('albumArtist')
+#         trackAlbum = song.get('album')
+#         trackNumber = song.get('track')
+
+for root, dirs, files in os.walk(music_path):
+    print files
