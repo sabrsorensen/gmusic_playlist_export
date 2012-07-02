fd06d36f623f30c0f9bc9399fc2ce24da6e9b532
Murph Murphy <omurcada@gmail.com>
2012-07-01 19:28:32 -0600

    Usability changes.

--- gmusic_playlist_export.py
+++ gmusic_playlist_export.py
@@ -1,26 +1,31 @@
 import gmusicapi
-#from local_constants import account, password, music_path
 from file_management import pathfinder
 from m3u_generation import M3U_Gen
 from getpass import getpass
 
 api = gmusicapi.Api()
 
-account = raw_input('Input your Google Music account:')
+account = raw_input('Input your Google Music account: ')
 password = getpass("Password: ")
-musicRoot = raw_input('Enter the location of your music library:')
-api.login(account, password)
+try:
+    api.login(account, password)
+except api.NotLoggedIn:
+    print("Your account details didn't check out. Please restart the script and try again.")
+music_root = raw_input('Enter the location of your music library: ')
 
 userlists = api.get_all_playlist_ids().get('user')
 listnames = userlists.keys()
 
 for name in listnames:
-    playGen = M3U_Gen(name,musicRoot)
+    print("Exporting playlist " + name + "...")
+    playGen = M3U_Gen(name, music_root)
     songs = api.get_playlist_songs(userlists.get(name))
     for song in songs:
-        path = pathfinder(musicRoot, song)
+        path = pathfinder(music_root, song)
         if path is None:
             continue
         playGen.M3UEntry(path)
 
     playGen.M3UClose()
+
+print("Your playlists have been exported to " + music_root + "GMusic Exported Playlists, happy listening!")
