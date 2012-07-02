2b1001c737b47206f533854651ceca22d3cc4140
Sam Sorensen <sabrsorensen@gmail.com>
2012-07-01 18:52:13 -0600

    Cleaning up the code.

--- gmusic_playlist_export.py
+++ gmusic_playlist_export.py
@@ -1,14 +1,15 @@
 import gmusicapi
-from local_constants import account, password, music_path
+#from local_constants import account, password, music_path
 from file_management import pathfinder
 from m3u_generation import M3U_Gen
+from getpass import getpass
 
 api = gmusicapi.Api()
 
-#from getpass import getpass
-#account = raw_input('Input your Google Music account:')
-#password = getpass("Password: ")
-#musicRoot = raw_input('Enter the location of your music library:')
+
+account = raw_input('Input your Google Music account:')
+password = getpass("Password: ")
+musicRoot = raw_input('Enter the location of your music library:')
 api.login(account, password)
 
 playlists = api.get_all_playlist_ids()
@@ -20,11 +21,11 @@ listids = userlists.values()
 music_path = music_path
 
 for name in listnames:
-    playGen = M3U_Gen(name)
+    playGen = M3U_Gen(name,music_path)
     songs = api.get_playlist_songs(userlists.get(name))
     for song in songs:
         path = pathfinder(music_path, song)
-        if path == None:
+        if path is None:
             continue
         playGen.M3UEntry(path)
 
