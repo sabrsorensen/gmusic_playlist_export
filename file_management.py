dd28c2eee6491eef58c4e3b9f514b9814394990e
Sam Sorensen <sabrsorensen@gmail.com>
2012-07-06 23:13:36 -0700

    User can now choose matches, better output
    Also implemented a pseudo-dynamic programming approach to finding songs
    previously found.

--- file_management.py
+++ file_management.py
@@ -4,34 +4,60 @@ import fnmatch
 from mutagen import flac, mp3, asf, ogg, mp4
 import mutagen
 
+class Finder:
+    def __init__(self):
+        self.history = {}
+        self.global_missing = {}
 
-def pathfinder(root, song):
-    '''Return a local path for the given song'''
-    foundMultiple = False
-    foundUnsupported = False
-    for path, directories, files in os.walk(os.path.abspath(root)):
-        for filename in fnmatch.filter(files, '*' + song.get('title') + '*'):#+ ('.mp3' or '.flac' or '.wma' or '.m4a' or '.ogg')):
-            if filename.endswith('.mp3') or filename.endswith('.flac') or filename.endswith('.ogg') or filename.endswith('.wma') or filename.endswith('.m4a'):
-                audio = mutagen.File(os.path.join(path, filename), easy=True)                
-                '''Old eyeD3 code, kept for archival purposes while testing mutagen code'''
-                #tag = eyeD3.Tag()
-                #tag.link(os.path.join(path, filename))
-                #if tag.getTitle() == song.get('title') and tag.getArtist() == song.get('artist') and tag.getAlbum() == song.get('album'):
-                if audio['title'][0] == song.get('title') and audio['artist'][0] == song.get('artist') and audio['album'][0] == song.get('album'):
-                    if foundMultiple or foundUnsupported:
-                        print "Multiple possibilities have been found. Accepting: %s - %s - %s" % (audio['artist'][0], audio['album'][0], audio['title'][0])
-                        print os.path.join(path, filename) + '\n'
-                        raw_input("Press Enter to continue...\n")
-                    return os.path.join(path, filename)
-                else:
-                    foundMultiple = True
-                    print "Song %s - %s - %s not found." % (song.get('artist'), song.get('album'), song.get('title'))
-                    print "Found %s - %s - %s instead.\n" % (audio['artist'][0], audio['album'][0], audio['title'][0])
-                    pass
-            else:
-                foundUnsupported = True
-                print "Found %s." % filename
-                print "Found no audio file matching %s - %s - %s. Other filetypes unsupported at this time.\n" % (song.get('artist'), song.get('album'), song.get('title'))
+    def print_missing(self):
+        print "\nMissing tracks in the following playlists:"
+        for playlist in self.global_missing.keys():
+            print "\nPlaylist %s:\n" % playlist
+            for track in self.global_missing[playlist]:
+                print "     %s" % track
+        print "Downloading is unsupported at this time, use the official Google Music Manager or otherwise obtain a local copy.\n"
 
-    print "Song %s - %s - %s not found locally." % (song.get('artist'), song.get('album'), song.get('title'))
-    raw_input("Press Enter to continue...\n")
+    def pathfinder(self, root, song, name):
+        '''Return a local path for the given song'''
+        foundMultiple = False
+        foundUnsupported = False
+        results = []
+        if name not in self.global_missing:
+            playlist_missing = []
+        else:
+            playlist_missing = self.global_missing[name]
+        songInfo = "%s - %s - %s" % (song.get('artist'), song.get('album'), song.get('title'))
+        if songInfo in self.history:
+            return self.history[songInfo]
+        for path, directories, files in os.walk(os.path.abspath(root)):
+            for filename in fnmatch.filter(files, '*' + song.get('title') + '*'):
+                if filename.endswith('.mp3') or filename.endswith('.flac') or filename.endswith('.ogg') or filename.endswith('.wma') or filename.endswith('.m4a'):
+                    audio = mutagen.File(os.path.join(path, filename), easy=True)
+                    if audio['title'][0] == song.get('title') and audio['artist'][0] == song.get('artist') and audio['album'][0] == song.get('album'):
+                        if foundMultiple:
+                            print "Multiple possibilities have been found. Perfect match located: %s - %s - %s" % (audio['artist'][0], audio['album'][0], audio['title'][0])
+                            print os.path.join(path, filename) + '\n'
+                            #raw_input("Press Enter to continue...\n")
+                        self.history[songInfo] = os.path.join(path, filename)
+                        return os.path.join(path, filename)
+                    else:
+                        foundMultiple = True
+                        results.append(os.path.join(path,filename))
+        if foundMultiple and len(results) > 1:
+            print "\nFuzzy or multiple possible matches found for %s, please select from the following:\n" % songInfo
+            resultCount = 1
+            for match in results:
+                print "[%d]: %s\n" % (resultCount, results[resultCount-1])
+                resultCount += 1
+            choice = raw_input("Enter your selection:")
+            while (int(choice) > resultCount or int(choice) < 1):
+                print "Invalid selection, try again.\n"    
+                choice = raw_input("Enter your selection:")
+            self.history[songInfo] = results[int(choice)-1]
+            return results[int(choice)-1]
+        if foundMultiple and len(results) == 1:
+            self.history[songInfo] = results[0]
+            return results[0]
+
+        playlist_missing.append(songInfo)
+        self.global_missing[name] = playlist_missing
--- gmusic_playlist_export.py
+++ gmusic_playlist_export.py
@@ -1,6 +1,6 @@
 import gmusicapi
 import os
-from file_management import pathfinder
+from file_management import Finder
 from m3u_generation import M3U_Gen
 from getpass import getpass
 
@@ -21,16 +21,19 @@ while not os.path.isdir(music_root):
 userlists = api.get_all_playlist_ids().get('user')
 listnames = userlists.keys()
 
+finder = Finder()
+
 for name in listnames:
     print "Exporting playlist %s..." % name
     playGen = M3U_Gen(name, music_root)
     songs = api.get_playlist_songs(userlists.get(name))
     for song in songs:
-        path = pathfinder(music_root, song)
+        path = finder.pathfinder(music_root, song, name)
         if path is None:
             continue
         playGen.M3UEntry(path)
     playGen.M3UClose()
 
+finder.print_missing()
 print "Your playlists have been exported to %s\\GMusic Exported Playlists\\, happy listening!" % music_root
 api.logout()
